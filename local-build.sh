b4cfebf0825dd3ce4a506284c2d860d760fcb802
Michael Walker <mike@barrucadu.co.uk>
2024-12-09 19:36:45 +0000

    Add (wip, proof-of-concept) summary pages

--- local-build.sh
+++ local-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nix develop --command bash -c "./build --drafts --root=https://misc.barrucadu.co.uk/_site/ --out=$HOME/http/_site"
+nix develop --command bash -c "./build --drafts $* --root=https://misc.barrucadu.co.uk/_site/ --out=$HOME/http/_site"
